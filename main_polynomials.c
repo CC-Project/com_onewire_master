77b2ef3bdad23cdf291d311be17ff038a313e2c6
herbertz0 <howherbertzo@gmail.com>
2015-06-18 18:58:36 +0200

    Poly update

--- main_polynomials.c
+++ main_polynomials.c
@@ -8,24 +8,27 @@
 #include "../lib_data/data.h"
 #include "../lib_uart/uart.h"
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
 #include <stdio.h>
-
+const uint16_t syndrome[N+1] PROGMEM = { 0, 1, 2, 4, 3, 7, 5, 6 };
 Poly generator;
 
 void main(void)
 {
+	poly_init_generator();
 	Poly data = data_generate(K);
-	data_set(0, 1, data);
-	data_set(2, 1, data);
-	 
-	Poly encoded = poly_encode(data);
 	
-    while(1)
-    {
-        for(uint8_t i = 0; i < N; i++){
-            ow_write_bit(data_get(i,encoded));
-			_delay_us(50);
-		}
-        _delay_ms(5000);
-    }
+	//data_set(0, 1, data);
+	//data_set(2, 1, data);
+	 //
+	//Poly encoded = poly_encode(data);
+	//
+    //while(1)
+    //{
+        //for(uint8_t i = 0; i < N; i++){
+            //ow_write_bit(data_get(i,encoded));
+			//_delay_us(50);
+		//}
+        //_delay_ms(5000);
+    //}
 }
